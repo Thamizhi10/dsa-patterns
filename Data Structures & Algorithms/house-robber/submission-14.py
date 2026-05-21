46fb28c3d627965a74dcaa8f0c8f1c4c17f568f2
Thamizhi10 <thamizhishanmugam@gmail.com>
2026-05-21 13:16:58 +0530

    Add: house-robber - submission-14

---
+++ Data Structures & Algorithms/house-robber/submission-14.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        first,second=0,0
+        for n in nums:
+            curr=max(first+n,second)
+            first=second
+            second=curr
+        return second
+
